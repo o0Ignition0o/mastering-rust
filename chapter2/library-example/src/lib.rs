9931ffde737c239db5fac07c588c1ee30f352a40
o0Ignition0o <jeremy.lempereur@gmail.com>
2017-09-03 17:21:18 +0200

    Chapter two is about configuring the IDE, I went for emacs, so there isn't much to show here :/

---
+++ chapter2/library-example/src/lib.rs
@@ -0,0 +1,13 @@
+#[allow(unused_variables)]
+fn sum(a: i8, b: i8) -> i8 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::sum;
+    #[test]
+    fn sum_one_and_one_equals_two() {
+        assert_eq!(sum(1, 1), 2);
+    }
+}
